091be536568fd6ebb6241ee0aff2c70996dab867
Daniel <daniel.a001@edu.pucrs.br>
2025-01-04 16:09:04 -0300

    inicializar repositorio do projeto

---
+++ src/main/java/com/agil/gerenciamento_agilstore/GerenciamentoAgilstoreApplication.java
@@ -0,0 +1,13 @@
+package com.agil.gerenciamento_agilstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAgilstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoAgilstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agil/gerenciamento_agilstore/GerenciamentoAgilstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agil.gerenciamento_agilstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoAgilstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
